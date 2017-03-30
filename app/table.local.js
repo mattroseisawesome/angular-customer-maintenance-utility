8200f8e2b483c187c07169c1b873f175edfbaa51
Matt Rose <matt.rose.email@gmail.com>
2017-03-30 16:17:09 -0400

    Validation updates
    Fixed some validation issues, added new success message

--- app/table.local.js
+++ app/table.local.js
@@ -1,61 +1,70 @@
 'use strict';
 
 var app = angular.module('customerAdminApp', []);
-app.controller('customerAppController', ['$scope', function ($scope) {
+app.controller('customerAppController', ['$scope','$timeout', function ($scope, $timeout) {
     $scope.saved = localStorage.getItem('customerDetails');
     $scope.customerDetails = (localStorage.getItem('customerDetails') !== null) ? JSON.parse($scope.saved) : [
         {
-            emailAddress : 'ckent@dailyplanet.com',
-            firstName : 'Clark',
-            lastName : 'Kent',
-            phoneNumber : '434-215-1938',
-            streetAddress : '',
+            emailAddress: 'ckent@dailyplanet.com',
+            firstName: 'Clark',
+            lastName: 'Kent',
+            phoneNumber: '434-215-1938',
+            streetAddress: '',
             modified: false
         },
         {
-            emailAddress : 'bwayne@wayneenterprises.com',
-            firstName : 'Bruce',
-            lastName : 'Wayne',
-            phoneNumber : '272-720-1939',
-            streetAddress : '7273 Palasaides Blvd, Gotham City, NY 10203',
+            emailAddress: 'bwayne@wayneenterprises.com',
+            firstName: 'Bruce',
+            lastName: 'Wayne',
+            phoneNumber: '272-720-1939',
+            streetAddress: '7273 Palasaides Blvd, Gotham City, NY 10203',
             modified: false
         },
         {
-            emailAddress : 'diana@themyscira.gov',
-            firstName : 'Diana',
-            lastName : 'Prince',
-            phoneNumber : '',
-            streetAddress : '',
+            emailAddress: 'diana@themyscira.gov',
+            firstName: 'Diana',
+            lastName: 'Prince',
+            phoneNumber: '',
+            streetAddress: '',
             modified: false
         },
         {
-            emailAddress : 'hal.jordan@usaf.gov',
+            emailAddress: 'hal.jordan@usaf.gov',
             firstName: 'Hal',
             lastName: 'Jordan',
             phoneNumber: '514-651-1959',
-            streetAddress : '',
+            streetAddress: '',
             modified: false
         },
         {
-            emailAddress : 'barry_allen@ccpd.gov',
-            firstName : 'Barry',
-            lastName : 'Allen',
-            phoneNumber : '',
-            streetAddress : '',
+            emailAddress: 'barry_allen@ccpd.gov',
+            firstName: 'Barry',
+            lastName: 'Allen',
+            phoneNumber: '',
+            streetAddress: '',
             modified: false
         }
     ];
+
+    $scope.display = function() {
+      $scope.displaySuccess = true;
+      $timeout(function() {
+        $scope.displaySuccess = false;
+      }, 1500)
+    };
+    //Customer Info form
     $scope.addNew = function (customerDetail) {
         $scope.customerDetails.push({
-            'emailAddress' : customerDetail.emailAddress,
-            'firstName' : customerDetail.firstName,
-            'lastName' : customerDetail.lastName,
-            'phoneNumber' : customerDetail.phoneNumber,
-            'streetAddress' : customerDetail.streetAddress
+            'emailAddress': customerDetail.emailAddress,
+            'firstName': customerDetail.firstName,
+            'lastName': customerDetail.lastName,
+            'phoneNumber': customerDetail.phoneNumber,
+            'streetAddress': customerDetail.streetAddress
         });
         localStorage.setItem('customerDetails', JSON.stringify($scope.customerDetails));
-
-        /* reset values for the add user form upon successful submission */
+        $scope.displayMessage = "Record added successfully!";
+        
+       // Reset values for the "Customer Info" form upon successful submission
         $scope.customerDetail.emailAddress = null;
         $scope.customerDetail.firstName = null;
         $scope.customerDetail.lastName = null;
@@ -65,12 +74,16 @@ app.controller('customerAppController', ['$scope', function ($scope) {
         $scope.addUser.$setUntouched();
     };
 
+    //Save changes to current records
     $scope.save = function (index) {
         var state = JSON.parse(localStorage.getItem('customerDetails'));
         state[index] = $scope.customerDetails[index];
         localStorage.setItem('customerDetails', JSON.stringify(state));
+        $scope.displayMessage = "Record saved successfully!";
+
     };
 
+    //Get index of record, set form to "dirty" if a checkbox is selected.  I do this because the ng-model/ng-checked pairing is weird with setting dirty.
     $scope.report = function (customerDetail, index) {
         $scope.customerDetails[index].selected = !$scope.customerDetails[index].selected;
         if ($scope.customerDetails[index].selected === true) {
@@ -78,6 +91,7 @@ app.controller('customerAppController', ['$scope', function ($scope) {
         }
     };
 
+    //Check all the things!!
     $scope.checkAll = function () {
         if (!$scope.selectedAll) {
             $scope.selectedAll = true;
@@ -88,6 +102,8 @@ app.controller('customerAppController', ['$scope', function ($scope) {
             customerDetail.selected = $scope.selectedAll;
         });
     };
+
+    //Remove record(s)
     $scope.remove = function () {
         var newones = [];
         angular.forEach($scope.customerDetails, function (customerDetail) {
@@ -97,6 +113,7 @@ app.controller('customerAppController', ['$scope', function ($scope) {
         });
         $scope.customerDetails = newones;
         localStorage.setItem('customerDetails', JSON.stringify($scope.customerDetails));
+        $scope.displayMessage = "Record removed successfully!";
     };
 
 }]);
